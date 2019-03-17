387b72fdac8ea8ddb638a9716b5973d14439185b
Teoman <teomanaydogan2003@gmail.com>
2019-03-18 08:06:23 +1100

    First Commit

---
+++ Cydia/PackageManager.swift
@@ -0,0 +1,18 @@
+//
+//  PackageManager.swift
+//  Cydia
+//
+//  Created by Teoman Aydogan on 18/3/19.
+//  Copyright © 2019 Teoman Aydogan. All rights reserved.
+//
+
+import Foundation
+
+var package_name = ""
+var package_desc = ""
+var package_authr = ""
+var package_wnew = ""
+var package_updated = ""
+var package_license = ""
+var package_ver = ""
+
---
+++ Cydia/UserManager.swift
@@ -0,0 +1,16 @@
+//
+//  UserManager.swift
+//  Cydia
+//
+//  Created by Teoman Aydogan on 18/3/19.
+//  Copyright © 2019 Teoman Aydogan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+var firstname = "John"
+var lastname = "Appleased"
+var name = firstname + " " + lastname
+var email = "johnappleased@email.com"
+
