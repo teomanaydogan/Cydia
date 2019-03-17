806f646047d85d4c750af6bedb3b4fc568d1735a
Teoman Aydogan <teomanaydogan@teomans-mac-pro.local>
2019-03-17 22:04:37 +1100

    Initial Commit

---
+++ Cydia/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Cydia
+//
+//  Created by Teoman Aydogan on 17/3/19.
+//  Copyright © 2019 Teoman Aydogan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
